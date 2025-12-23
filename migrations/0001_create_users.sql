df729e999ded9644d9c93afffcc3fd5838e7aba6
Cyphle <cyril.phamle@gmail.com>
2025-12-23 11:54:34 +0100

    create and get user

--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -1,5 +1,5 @@
 -- Creates the `users` table used in examples/tests
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER,
-    username VARCHAR(255)
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL
 );

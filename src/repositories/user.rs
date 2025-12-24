aeb01afbe2b63ed7d6a7423ccad09ab50ee16703
Cyphle <cyril.phamle@gmail.com>
2025-12-24 10:07:17 +0100

    user repository

--- src/repositories/user.rs
+++ src/repositories/user.rs
@@ -8,55 +8,67 @@ struct UserEntity {
     username: String,
 }
 
-// Create user
-async fn create_user<'e, E>(pool: E, user: &User) -> Result<(u64, StatusCode), sqlx::Error>
-where
-    E: Executor<'e, Database = Postgres>,
-{
-    let row: (i32,) = sqlx::query_as("INSERT INTO users (username) VALUES ($1) RETURNING id")
-        .bind(&user.username)
-        .fetch_one(pool)
-        .await?;
-
-    Ok((row.0 as u64, StatusCode::CREATED))
+trait UserRepository {
+    async fn create_user<'e, E>(pool: E, user: &User) -> Result<(u64, StatusCode), sqlx::Error> where E: Executor<'e, Database = Postgres>;
+    async fn get_user<'e, E>(pool: &Pool<Postgres>, username: String) -> Result<UserEntity, sqlx::Error> where E: Executor<'e, Database = Postgres>;
+    async fn get_or_create_user<'e, E>(exec: E, user: &User) -> Result<UserEntity, sqlx::Error> where E: sqlx::Executor<'e, Database = Postgres>;
 }
 
-// Get user
-async fn get_user<'e, E>(pool: &Pool<Postgres>, username: String) -> Result<UserEntity, sqlx::Error>
-where
-    E: Executor<'e, Database = Postgres>,
-{
-    // Postgres uses positional parameters like $1
-    let row = sqlx::query_as::<sqlx::Postgres, UserEntity>(
-        "SELECT id FROM users WHERE username = $1 LIMIT 1",
-    )
-    .bind(username)
-    .fetch_one(pool)
-    .await?;
-
-    Ok(row)
+struct SqlxUserRepository {
 }
 
-// Upsert user
-pub async fn get_or_create_user<'e, E>(
-    exec: E,
-    user: &User,
-) -> Result<UserEntity, sqlx::Error>
-where
-    E: sqlx::Executor<'e, Database = Postgres>,
-{
-    let user = sqlx::query_as::<Postgres, UserEntity>(
-        r#"
+impl UserRepository for SqlxUserRepository {
+    // Create user
+    async fn create_user<'e, E>(pool: E, user: &User) -> Result<(u64, StatusCode), sqlx::Error>
+    where
+        E: Executor<'e, Database = Postgres>,
+    {
+        let row: (i32,) = sqlx::query_as("INSERT INTO users (username) VALUES ($1) RETURNING id")
+            .bind(&user.username)
+            .fetch_one(pool)
+            .await?;
+
+        Ok((row.0 as u64, StatusCode::CREATED))
+    }
+
+    // Get user
+    async fn get_user<'e, E>(pool: &Pool<Postgres>, username: String) -> Result<UserEntity, sqlx::Error>
+    where
+        E: Executor<'e, Database = Postgres>,
+    {
+        // Postgres uses positional parameters like $1
+        let row = sqlx::query_as::<sqlx::Postgres, UserEntity>(
+            "SELECT id FROM users WHERE username = $1 LIMIT 1",
+        )
+            .bind(username)
+            .fetch_one(pool)
+            .await?;
+
+        Ok(row)
+    }
+
+    // Upsert user
+    pub async fn get_or_create_user<'e, E>(
+        exec: E,
+        user: &User,
+    ) -> Result<UserEntity, sqlx::Error>
+    where
+        E: sqlx::Executor<'e, Database = Postgres>,
+    {
+        let user = sqlx::query_as::<Postgres, UserEntity>(
+            r#"
         INSERT INTO users (username)
         VALUES ($1)
         ON CONFLICT (username)
         DO UPDATE SET username = EXCLUDED.username
         RETURNING id, username
         "#
-    )
-        .bind(&user.username)
-        .fetch_one(exec)
-        .await?;
+        )
+            .bind(&user.username)
+            .fetch_one(exec)
+            .await?;
 
-    Ok(user)
+        Ok(user)
+    }
 }
+

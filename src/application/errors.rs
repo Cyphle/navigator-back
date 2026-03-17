40b0e76c00e02d6be4128b25975444427077f92f
Cyphle <cyril.phamle@gmail.com>
2026-03-17 15:26:26 +0100

    dashboard middleware

--- src/application/errors.rs
+++ src/application/errors.rs
@@ -1,3 +1,5 @@
+// TODO c'est pas besoin cette gestion d'erreur centralisée parce que tout le monde doit traiter tous les cas
+// Il faut réfléchir à un générique et que les autres étendent ou alors on s'en fout
 #[derive(Debug)]
 pub enum ApplicationErrors {
     FamilyAlreadyExists,
--- src/http/middlewares/mod.rs
+++ src/http/middlewares/mod.rs
@@ -1,2 +1,3 @@
 pub mod family;
 pub mod user;
+mod dashboard;
